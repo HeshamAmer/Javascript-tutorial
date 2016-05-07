12198f5efa456f4194c5a84dd9417f4dc469efee
Hesham Amer <HeshamHamdyAmer@gmail.com>
2016-05-07 23:53:14 +0200

    Pushing files

---
+++ Todo list app/js/src.js
@@ -0,0 +1,10 @@
+/**
+ * Created by Gedy on 5/7/2016.
+ */
+var Lists={};
+var inputText=document.getElementById("input");
+var btn=document.getElementById("btn");
+function addToList(){
+
+    console.log("input inserted is:" + inputText   );
+}
---
+++ publicLibrary/src/model/Book-data.js
@@ -0,0 +1,3 @@
+{ isbn:"006251587X," title:"Weaving the Web", year:2000 }
+{ isbn:"0465026567," title:"Gödel, Escher, Bach", year:1999 }
+{ isbn:"0465030793," title:"I Am A Strange Loop", year:2008 }
