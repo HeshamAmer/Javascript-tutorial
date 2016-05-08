75d506df337152022e76e0fde3d6a9ae5fab625d
Hesham Amer <HeshamHamdyAmer@gmail.com>
2016-05-09 01:12:56 +0200

    Lists now can have swapped elements.

--- Todo list app/js/src.js
+++ Todo list app/js/src.js
@@ -1,10 +1,35 @@
 /**
  * Created by Gedy on 5/7/2016.
  */
-var Lists={};
-var inputText=document.getElementById("input");
-var btn=document.getElementById("btn");
-function addToList(){
+var idCounter=0;
+function addToList(listName){
 
-    console.log("input inserted is:" + inputText   );
+    var Lists={
+        todo:document.getElementById('todo'),
+        done:document.getElementById('done')
+    };
+    var inputText=document.getElementById("input").value.trim();
+
+    if (inputText.length>0) {
+        var btn = document.getElementById("btn");
+
+        var li = document.createElement("li");
+
+        var chkbox = document.createElement("input");
+        chkbox.setAttribute("type", "checkbox");
+
+        chkbox.addEventListener('click', function () {
+            var list = this.parentElement.parentElement.id; //Todo or Done
+            Lists[list === 'done' ? 'todo' : 'done'].appendChild(this.parentElement);
+            this.checked = false;
+            //console.log(this.parentElement.parentElement.id);
+        });
+        li.appendChild(chkbox);
+        li.appendChild(document.createTextNode(inputText));
+        var ID = Lists[listName].length;
+        li.setAttribute("id", idCounter++);
+        Lists[listName].appendChild(li);
+        console.log("input inserted is:" + inputText);
+    }
 }
+
