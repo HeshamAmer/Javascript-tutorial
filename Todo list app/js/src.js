90e53b444bacea30df178f9bf2af3b907ac36315
Hesham Amer <HeshamHamdyAmer@gmail.com>
2016-05-11 00:10:03 +0200

    Tasks now have drop down lists, will start to fix colors

--- Todo list app/js/src.js
+++ Todo list app/js/src.js
@@ -14,7 +14,6 @@ function addToList(listName){
         var btn = document.getElementById("btn");
 
         var li = document.createElement("li");
-
         var chkbox = document.createElement("input");
         chkbox.setAttribute("type", "checkbox");
 
@@ -25,9 +24,26 @@ function addToList(listName){
             //console.log(this.parentElement.parentElement.id);
         });
         li.appendChild(chkbox);
-        li.appendChild(document.createTextNode(inputText));
-        var ID = Lists[listName].length;
-        li.setAttribute("id", idCounter++);
+        var textChild= document.createElement("span");
+        textChild.innerHTML=inputText;
+        li.appendChild(textChild);
+            var ID = Lists[listName].length;
+            li.setAttribute("id", idCounter++);
+        var Select = document.createElement("select");
+        Select.id = "sel1";
+        Select.name="sel1";
+
+        var Colors=["red","green","blue"];
+        for (var i=0;i<Colors.length;i++){
+            var Option1=  document.createElement('option');
+            Option1.setAttribute("value",Colors[i]);
+            Option1.setAttribute("text",Colors[i]);
+            Option1.innerText=Colors[i];
+            Select.appendChild(Option1);
+        }
+//Kewl :D
+
+        li.appendChild(Select);
         Lists[listName].appendChild(li);
         console.log("input inserted is:" + inputText);
     }
