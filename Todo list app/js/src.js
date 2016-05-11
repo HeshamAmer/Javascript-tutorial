43f5a5a72011d0573196878dd06039b98457ceb3
Hesham Amer <HeshamHamdyAmer@gmail.com>
2016-05-12 00:28:04 +0200

    added motivation

--- Todo list app/js/src.js
+++ Todo list app/js/src.js
@@ -25,27 +25,50 @@ function addToList(listName){
         });
         li.appendChild(chkbox);
         var textChild= document.createElement("span");
+        var ID = idCounter;
         textChild.innerHTML=inputText;
+        textChild.setAttribute("id","span"+idCounter)
         li.appendChild(textChild);
-            var ID = Lists[listName].length;
-            li.setAttribute("id", idCounter++);
+
+        li.setAttribute("id", idCounter++);
+
         var Select = document.createElement("select");
         Select.id = "sel1";
         Select.name="sel1";
 
-        var Colors=["red","green","blue"];
-        for (var i=0;i<Colors.length;i++){
+        var Colors=["black","red","green","blue"];
+
+
+            for (var i=0;i<Colors.length;i++){
             var Option1=  document.createElement('option');
             Option1.setAttribute("value",Colors[i]);
             Option1.setAttribute("text",Colors[i]);
             Option1.innerText=Colors[i];
-            Select.appendChild(Option1);
-        }
-//Kewl :D
+            Select.onchange=function (){
+                var id=this.parentElement.getAttribute('id');
+                id="span"+id;
+                document.getElementById(id).style.color=Select.options[Select.selectedIndex].value;
+                console.log("I'm in the Select event listner");
+            }
 
+            Select.appendChild(Option1);
+
+        }
         li.appendChild(Select);
+        li.textContent.fontcolor("red");
         Lists[listName].appendChild(li);
         console.log("input inserted is:" + inputText);
+        document.getElementById('input').value="";
     }
 }
 
+function handleImage(){
+    var img=document.getElementById("im1");
+    img.onmouseover =function(){
+        img.setAttribute("src","images/12552904_1933384730220566_167193102920467850_n.jpg");
+    }
+    img.onmouseout = function(){
+        img.setAttribute("src","images/352810-celeste-star.jpg");
+    }
+
+}
